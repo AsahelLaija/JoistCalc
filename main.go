289841b519cb4fba7094578b2445a3f33b4a3a42
Asahel <asahellaija@gmail.com>
2024-03-27 17:05:35 +0000

    styles pretty done and, golang air and echo framework up and running

--- main.go
+++ main.go
@@ -1,15 +1,21 @@
 package main
 import (
-  "log"
-  "net/http"
+    "github.com/labstack/echo/v4"
+    "github.com/labstack/echo/v4/middleware"
 )
 func main() {
-  fs := http.FileServer(http.Dir("./static"))
-  http.Handle("/", fs)
+    e := echo.New()
 
-  log.Print("Listening on :3000...")
-  err := http.ListenAndServe(":3000", nil)
-  if err != nil {
-    log.Fatal(err)
-  }
-}
+    e.Use(middleware.Logger())
+    e.Use(middleware.Recover())
+
+    e.GET("/", func(c echo.Context) error {
+	return c.File("static/index.html")
+    })
+    e.GET("/styles", func(c echo.Context) error {
+	return c.File("static/styles.css")
+    })
+
+
+    e.Logger.Fatal(e.Start(":8080"))
+} 
